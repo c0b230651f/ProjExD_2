75778030fc8e60ba3b7d8117fb6d9ef54060f26b
c0b230651f <c0b230651f@edu.teu.ac.jp>
2024-10-01 14:14:37 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,11 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA = {pg.K_UP:(0, -5),
+         pg.K_DOWN:(0, +5),
+         pg.K_LEFT:(-5, 0),
+         pg.K_RIGHT:(+5, 0),
+         }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -14,6 +19,7 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    # bb_img = 
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -24,14 +30,18 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
+        for key, tpl in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += tpl[0]  # 横方向
+                sum_mv[1] += tpl[1]  # 縦方向
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
